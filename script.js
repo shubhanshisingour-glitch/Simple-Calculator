4d95b3471487b300f9acf55be4593675d04b26da
Shubhanshi Singour <shubhanshisingour@gmail.com>
2026-06-15 15:47:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const display = document.getElementById("display");
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        let value = button.innerText;
+
+        if(value === "C"){
+            display.value = "";
+        }
+
+        else if(value === "="){
+
+            try{
+                display.value = eval(display.value);
+            }
+
+            catch{
+                display.value = "Error";
+            }
+
+        }
+
+        else{
+            display.value += value;
+        }
+
+    });
+
+});
